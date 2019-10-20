5cb272b439116903d20b8b68d91cad29ebecf147
Rishu Anand <riuandg5@gmail.com>
2019-10-20 11:51:28 +0530

    add dl model

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -1,5 +1,21 @@
+import math from "math.js";
+
+function breakUrl(url) {
+    return [url.match(/:\/\/(?:www\.)?(.[^/]+)(.*)/), url.match(/:\/\/(?:www\.)?(.[^/]+)(.*)/)]
+}
+
+function activation_function(num) {
+    return num >= 0;
+}
+
+W = [-40.34435, -35.34545];
+B = 1.544435;
+
 function checkUrl(url) {
-    return url === "https://thoughtbot.com/blog/how-to-make-a-chrome-extension"
+    inp = breakUrl(url);
+    res = math.dot(W, math.transpose(inp)) + B
+    res = activation_function(res)
+    return res;
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
